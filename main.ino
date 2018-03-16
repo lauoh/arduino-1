87a3121535627645fcd286701fe635f58309541c
lauoh <shanonnetlauren@gmail.com>
2018-03-16 16:44:51 +0100

    Arduino2 fini

--- main.ino
+++ main.ino
@@ -12,6 +12,9 @@ void setup()
 // the loop function runs over and over again forever
 void loop()
 {
+    static int potPosition;
+    potPosition = analogRead(A0);
+    
     digitalWrite(13, HIGH);
     delay(2000);
     digitalWrite(13, LOW);
