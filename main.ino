f9c884d8f968ce477184abe7e6cbc8ab4ac80c08
lauoh <shanonnetlauren@gmail.com>
2018-05-11 17:39:56 +0200

    TP3 FINI

--- main.ino
+++ main.ino
@@ -6,9 +6,50 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode (2 , INPUT_PULLUP);
+    pinMode (4 , INPUT_PULLUP);
+    pinMode (6 , INPUT_PULLUP);
+    pinMode (8, INPUT_PULLUP);
+
+    pinMode (3 , OUTPUT);
+    pinMode (5 , OUTPUT);
+    pinMode (7 , OUTPUT);
+    pinMode (9 , OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-}
+    if (digitalRead(2) ==LOW)
+    {
+        digitalWrite(3,LOW);
+    }
+    else
+    {
+        digitalWrite(3, HIGH);
+    }
+    if (digitalRead (4) ==LOW)
+    {
+        digitalWrite(5,LOW);
+    }
+    else
+    {
+        digitalWrite(5, HIGH);
+    }
+    if (digitalRead (6) ==LOW)
+    {
+        digitalWrite(7,LOW);
+    }
+    else
+    {
+        digitalWrite(7, HIGH);
+    }
+    if (digitalRead (8) ==LOW)
+    {
+        digitalWrite(9,LOW);
+    }
+    else
+    {
+        digitalWrite(9, HIGH);
+    }
+ }
